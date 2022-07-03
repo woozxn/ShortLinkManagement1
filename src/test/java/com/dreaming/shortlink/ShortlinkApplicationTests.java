b70684f4bad6d15f5676f515f43d9c0b9f53cd58
hoyaspark <hoyaspark@gmail.com>
2022-07-03 16:24:42 +0900

    feat(master) : Init Project

---
+++ src/test/java/com/dreaming/shortlink/ShortlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreaming.shortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
